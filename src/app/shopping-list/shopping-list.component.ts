899342af174a8cde525abfbb3ee7790a7fa6995a
Matthew Waskin <matthew@waskin.com>
2023-07-17 16:52:15 -0400

    add ability to add ingredients to shopping list from the input field using @ViewChild

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -12,4 +12,8 @@ export class ShoppingListComponent {
     new Ingredient('Tomatoes', 10),
   ];
   @Input() isActive: boolean = false;
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient);
+  }
 }
