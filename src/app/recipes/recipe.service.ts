a574e9c7370b34ee05b6199d226e66e667c29c4b
Matthew Waskin <matthew@waskin.com>
2023-07-21 16:21:39 -0400

    add shopping list service to refactor app with leaner code; add ingredients to recipes; add functionality to sent recipe ingredients to shopping list

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,6 +1,9 @@
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
   recipeSelected = new EventEmitter<Recipe>();
 
@@ -8,17 +11,25 @@ export class RecipeService {
     new Recipe(
       'A Test Recipe',
       'This is simply a test',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [new Ingredient('Meat', 1), new Ingredient('French Fries', 20)]
     ),
     new Recipe(
       'A Test Recipe2',
       'This is simply a test2',
-      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg'
+      'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_1280.jpg',
+      [new Ingredient('Buns', 2), new Ingredient('Meat', 1)]
     ),
   ];
+
+  constructor(private slService: ShoppingListService) {}
 
   getRecipes() {
     // returns a copy of the array instead of a direct reference
     return this.recipes.slice();
+  }
+
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
 }
