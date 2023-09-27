53d307496d78f89244843f069ce474a6117cb44d
Matthew Waskin <matthew@waskin.com>
2023-09-27 10:43:20 -0400

    add generic error message to UI

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -9,6 +9,7 @@ import { AuthService } from './auth.service';
 export class AuthComponent {
   isLoginMode = true;
   isLoading = false;
+  error: string = null;
 
   constructor(private authService: AuthService) {}
 
@@ -34,6 +35,7 @@ export class AuthComponent {
         },
         (error) => {
           console.log(error);
+          this.error = 'An error occurred!';
           this.isLoading = false;
         }
       );
